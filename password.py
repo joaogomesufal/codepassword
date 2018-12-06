063eea639c917dc37bb9e530e1e78c1ab14a7feb
joaogomesufal <43356521+joaogomesufal@users.noreply.github.com>
2018-12-06 14:38:38 -0300

    Create password.py

---
+++ password.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+# python 2.7
+alpha = {} #Existe um alfabeto escondido dentro de alguma sala do bloco C. Procure e tente executar o código
+
+def gernerate(chars):
+	result = ''
+	for char in chars:
+		if char in alpha:
+			result += alpha[char]
+		else:
+			result += char
+	return result
+
+input_chars = raw_input("Insira uma sequencia de caracteres: \n")
+
+print(gernerate(input_chars)
